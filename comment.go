efe0b529e304c7ce347dfd13300b72b48f23a8de
Nelson J Morais <njmorais@proactive-hosters.com>
2015-05-02 13:21:48 +0100

    changes to .gitignore and minor changes

--- comment.go
+++ comment.go
@@ -1,14 +1,21 @@
 package gosound
 
+import (
+    "errors"
+    "strconv"
+    "encoding/json"
+)
+
 type Comment struct {
-    *resource
-    User_id    uint64
-    Track_id   uint64
-    Timestamp  uint64
-    Created_at string
-    Body       string "Html comment"
-    Uri        string
-    User       User
+    Id          uint64
+    Kind        string
+    User_id     uint64
+    Track_id    uint64
+    Timestamp   uint64
+    Created_at  string
+    Body        string "Html comment"
+    Uri         string
+    Creator     User
     /* user contains based on api docs
     "creator": {
         "id": 1433,
@@ -21,17 +28,29 @@ type Comment struct {
 }
 
 func NewComment() *Comment {
-    r := newResource()
-    c := &Comment{}
-    c.resource = r
-    return c
+    return &Comment{Kind:"comment"}
 }
 
-func (c *Comment) GetUserId() uint64 {
+func (c Comment) GetId() uint64 {
+    return c.Id
+}
+
+func (c Comment) GetKind() string {
+    return c.Kind
+}
+
+func (c Comment) IsNew() bool {
+    if c.Id > 0 {
+        return false
+    }
+    return true
+}
+
+func (c Comment) GetUserId() uint64 {
     return c.User_id
 }
 
-func (c *Comment) GetTrackId() uint64 {
+func (c Comment) GetTrackId() uint64 {
     return c.Track_id
 }
 
@@ -73,4 +92,37 @@ func (c *Comment) SetBody(b string) bool {
 
 func (c *Comment) GetUri() string {
     return c.Uri
+}
+
+func (c *Comment) Save(s *SoundcloudApi) error {
+    if !c.IsNew() {
+        return errors.New("Comment is not new, cannot be saved!")
+    }
+    if c.Track_id == 0 {
+        return errors.New("Track ID missing, cannot be saved!")
+    }
+
+    url := "/tracks/" + strconv.FormatUint(c.Track_id, 10) + "/comments"
+    _, err := s.Post(url, *c)
+    return err
+}
+
+func (c *Comment) Delete(s *SoundcloudApi) error {
+    if c.IsNew() {
+        return errors.New("Comment is new, cannot be deleted!")
+    }
+
+    url := "/comments/" + strconv.FormatUint(c.Id, 10)
+    _, err := s.Delete(url)
+    return err
+}
+
+func (c Comment) MarshalJSON() ([]byte, error) {
+    j := map[string]map[string]interface{}{
+        "comment": {
+            "body": c.Body,
+            "timestamp": c.Timestamp,
+        },
+    }
+    return json.Marshal(j)
 }
--- gosound.go
+++ gosound.go
@@ -1,46 +1,19 @@
 package gosound
 
 type Saver interface {
-	Save(s *SoundcloudApi)
+	Save(s *SoundcloudApi) error
 }
 
 type Updater interface {
-	Update(s *SoundcloudApi)
+	Update(s *SoundcloudApi) error
 }
 
 type Deleter interface {
-	Delete(s *SoundcloudApi)
+	Delete(s *SoundcloudApi) error
 }
 
 type Resourcer interface {
-	Id()    uint64
-	Kind()  string
-	IsNew() bool
+	GetId()     uint64
+	GetKind()   string
+	IsNew()     bool
 }
-
-type resource struct {
-	id   uint64
-	kind string
-}
-
-func (r *resource) Id() uint64 {
-	return r.id
-}
-
-func (r *resource) Kind() string {
-	return r.kind
-}
-
-func (r *resource) IsNew() bool {
-	if r.id > 0 {
-		return false
-	}
-	return true
-}
-
-func newResource() *resource {
-	return &resource{}
-}
-
-
-
